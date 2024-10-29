25f2d1f20250863778c064439181439a12623732
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-10-30 00:21:34 +0530

    fixed some bugs

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const connectDB = async () => {
   try {
+    console.log("MongoDB URI:", process.env.MONGODB_URI);
+    
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "BlogIt",
     });
-    console.log("Connected To DB");
+    
+    console.log("Connected to DB");
   } catch (error) {
-    console.log("Error connecting to DB: ", error);
+    console.error("Error connecting to DB:", error);
   }
 };
 
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import dotenv from "dotenv";
 import connectDB from "./db.js";
-
+dotenv.config();
+connectDB()
 import User from "./models/user.model.js";
 export { connectDB, User };
