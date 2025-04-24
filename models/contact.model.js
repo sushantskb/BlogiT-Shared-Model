cb77fbe83a1da23fd3b545a9caea9d0bcbfbbf62
sushantskb <sushantsrk2001@gmail.com>
2025-04-24 19:21:03 +0530

    added the contact model

---
+++ models/contact.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  subject: {
+    type: String,
+  },
+  message: {
+    type: String,
+  },
+});
